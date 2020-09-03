e4fa488cfc537d6a556dea3762b59042ed6169d5
Dora <55266839@qq.com>
2020-09-03 18:38:53 +0800

    no message

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,7 +27,7 @@ class AppServiceProvider extends ServiceProvider
         //扩展身份证验证规则
         Validator::extend('identitycards', function($attribute, $value, $parameters) {
             $dl = '/^\d{6}(18|19|20)?\d{2}(0[1-9]|1[012])(0[1-9]|[12]\d|3[01])\d{3}(\d|[xX])$/';
-            $xg = '/^(\s?[a-zA-Z0-9])\d{6}(\([a-zA-z0-9]\))$/';
+            $xg = '/^(\s?[a-zA-Z0-9])\d{6}(\([a-zA-Z0-9]\)|[a-zA-Z0-9])$/';
             $tw = '/^[a-zA-Z][0-9]{9}$/';
             $am = '/^[1|5|7][0-9]{6}\([0-9Aa]\)/';
             return preg_match($dl, $value) || preg_match($xg, $value) || preg_match($tw, $value) || preg_match($am, $value);
