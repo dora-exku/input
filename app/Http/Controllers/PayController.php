d2a1662448b6de404960c6853d9f98dd7d12b322
Dora <55266839@qq.com>
2020-09-03 14:08:09 +0800

    no message

--- app/Http/Controllers/PayController.php
+++ app/Http/Controllers/PayController.php
@@ -11,10 +11,16 @@ class PayController extends Controller
     public function view(Request $request, $order)
     {
         $info = Input::query()->where('no', $order)->first();
+        var_dump([
+            'out_trade_no' => $order,
+            'body' => '学生意外险',
+            'total_fee' => $info->total_amount * 100,
+            'openid' => $request->session()->get('openid')['openid'],
+        ]);
         $result = Pay::wechat()->mp([
             'out_trade_no' => $order,
             'body' => '学生意外险',
-            'total_fee' => $info->total_amount,
+            'total_fee' => $info->total_amount * 100,
             'openid' => $request->session()->get('openid')['openid'],
         ]);
         var_dump($result);
