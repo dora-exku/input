ae6545a3b64c8b1b4636d23ec6ce2acf3b47668e
Dora <55266839@qq.com>
2020-09-03 14:43:44 +0800

    no message

--- resources/views/pay/view.blade.php
+++ resources/views/pay/view.blade.php
@@ -39,7 +39,7 @@
         console.log('ok');
         $('#pay').click(function () {
             wx.chooseWXPay({
-                timestamp: {{ $info['timestamp'] }}, // 支付签名时间戳，注意微信jssdk中的所有使用timestamp字段均为小写。但最新版的支付后台生成签名使用的timeStamp字段名需大写其中的S字符
+                timestamp: {{ $info['timeStamp'] }}, // 支付签名时间戳，注意微信jssdk中的所有使用timestamp字段均为小写。但最新版的支付后台生成签名使用的timeStamp字段名需大写其中的S字符
                 nonceStr: '{{ $info['nonceStr'] }}', // 支付签名随机串，不长于 32 位
                 package: '{{ $info['package'] }}', // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=\*\*\*）
                 signType: '{{ $info['signType'] }}', // 签名方式，默认为'SHA1'，使用新版支付需传入'MD5'
